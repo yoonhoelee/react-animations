8b2e5eb9e2d4b8b6271e9b4baf7c9cd127fa9d12
Adam Lee <70354575+yoonhoelee@users.noreply.github.com>
2024-03-29 09:54:05 +0900

    feat: drag implemented

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const Wrapper = styled.div`
 const Box = styled(motion.div)`
     width: 200px;
     height: 200px;
-    background-color: rgba(255,255,255,0.2);
+    background-color: rgba(255,255,255,1);
     border-radius: 10px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
     display: grid;
@@ -29,21 +29,9 @@ const Circle = styled(motion.div)`
 `;
 
 const boxVariants = {
-    start:{
-        opacity:0,
-        scale:0.5,
-    },
-    end:{
-        opacity: 1,
-        scale: 1,
-        transition:{
-            type:"spring",
-            duration:0.5,
-            bounce:0.5,
-            delayChildren:0.5,
-            staggerChildren:0.2,
-        }
-    },
+    hover: {scale:1.5, rotateZ:90},
+    click: {scale: 1, borderRadius:"100px"},
+    drag: { backgroundColor: "rgb(46, 204, 113)", transition: { duration: 10 } },
 };
 
 const circleVariants = {
@@ -60,12 +48,7 @@ const circleVariants = {
 function App() {
     return (
         <Wrapper>
-            <Box variants={boxVariants} initial="start" animate="end">
-                <Circle variants={circleVariants}/>
-                <Circle variants={circleVariants}/>
-                <Circle variants={circleVariants}/>
-                <Circle variants={circleVariants}/>
-            </Box>
+            <Box drag variants={boxVariants} whileHover="hover" whileTap="click" whileDrag="drag"/>
         </Wrapper>
     );
 }
