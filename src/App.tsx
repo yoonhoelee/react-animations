6bb2e8236cd79596e2c7962ca10b2e0ef6324a2d
Adam Lee <70354575+yoonhoelee@users.noreply.github.com>
2024-04-04 08:21:41 +0900

    feat: using layout to animate multi-animation

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,29 @@ import {AnimatePresence, motion, useMotionValue, useTransform, useViewportScroll
 import {useEffect, useRef, useState} from "react";
 
 const Wrapper = styled(motion.div)`
-    height: 200vh;
+    height: 100vh;
     width: 100vw;
     display: flex;
-    justify-content: center;
+    justify-content: space-around;
     align-items: center;
-    flex-direction: column;
 `;
 
 const Box = styled(motion.div)`
-    width: 200px;
-    height: 200px;
+    width: 400px;
+    height: 400px;
     background-color: rgba(255, 255, 255, 1);
-    border-radius: 10px;
+    border-radius: 40px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
     display: flex;
     justify-content: center;
     align-items: center;
-    font-size: 28px;
-    grid-template-columns: repeat(2, 1fr);
 `;
 
 const Circle = styled(motion.div)`
+    background-color: #00a5ff;
+    height: 100px;
+    width: 100px;
     box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-    background-color: white;
-    height: 70px;
-    width: 70px;
-    border-radius: 35px;
-    place-self: center;
 `;
 
 const boxVariants = {
@@ -80,7 +75,7 @@ const svg = {
 
 const box = {
     entry: (isBack: boolean) => ({
-        x: isBack? -500:500,
+        x: isBack ? -500 : 500,
         opacity: 0,
         scale: 0,
     }),
@@ -95,7 +90,7 @@ const box = {
     },
 
     exit: (isBack: boolean) => ({
-        x: isBack? 500:-500,
+        x: isBack ? 500 : -500,
         opacity: 0,
         scale: 0,
         transition: {duration: 1}
@@ -107,6 +102,8 @@ function App() {
     const toggleShowing = () => setShowing((prev) => !prev);
     const [visible, setVisible] = useState(1);
     const [back, setBack] = useState(false);
+    const [clicked, setClicked] = useState(false);
+    const toggleClicked = () => setClicked((prev) => !prev);
     const nextPlease = () => {
         setBack(false);
         setVisible((prev) => (prev === 10 ? 10 : prev + 1));
@@ -117,21 +114,17 @@ function App() {
     };
 
     return (
-        <Wrapper>
-            <AnimatePresence custom={back}>
-                <Box
-                    custom={back}
-                    variants={box}
-                    initial="entry"
-                    animate="center"
-                    exit="exit"
-                    key={visible}
-                >
-                    {visible}
-                </Box>
-            </AnimatePresence>
-            <button onClick={nextPlease}>next</button>
-            <button onClick={prevPlease}>prev</button>
+        <Wrapper onClick={toggleClicked}>
+            <Box>
+                {!clicked ? (
+                    <Circle layoutId="circle" style={{ borderRadius: 50 }} />
+                ) : null}
+            </Box>
+            <Box>
+                {clicked ? (
+                    <Circle layoutId="circle" style={{ borderRadius: 0, scale: 2 }} />
+                ) : null}
+            </Box>
         </Wrapper>
     );
 }
