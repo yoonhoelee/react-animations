4077215361c807a45dff8816d0fe9c9c1fcf601b
Adam Lee <70354575+yoonhoelee@users.noreply.github.com>
2024-04-05 12:06:52 +0900

    feat: using framer motion with grid

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ const Wrapper = styled(motion.div)`
 `;
 
 const Box = styled(motion.div)`
-    width: 400px;
     height: 400px;
     background-color: rgba(255, 255, 255, 1);
     border-radius: 40px;
@@ -97,34 +96,60 @@ const box = {
     }),
 }
 
+const Grid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    width: 50vw;
+    gap: 10px;
+    div:first-child,
+    div:last-child {
+        grid-column: span 2;
+    }
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const overlay = {
+    hidden: { backgroundColor: "rgba(0, 0, 0, 0)" },
+    visible: { backgroundColor: "rgba(0, 0, 0, 0.5)" },
+    exit: { backgroundColor: "rgba(0, 0, 0, 0)" },
+};
+
 function App() {
     const [showing, setShowing] = useState(false);
     const toggleShowing = () => setShowing((prev) => !prev);
     const [visible, setVisible] = useState(1);
     const [back, setBack] = useState(false);
     const [clicked, setClicked] = useState(false);
-    const toggleClicked = () => setClicked((prev) => !prev);
-    const nextPlease = () => {
-        setBack(false);
-        setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-    };
-    const prevPlease = () => {
-        setBack(true);
-        setVisible((prev) => (prev === 1 ? 1 : prev - 1));
-    };
-
+    const toggle = () => setClicked((prev) => !prev);
+    const [id, setId] = useState<null | string>(null);
     return (
-        <Wrapper onClick={toggleClicked}>
-            <Box>
-                {!clicked ? (
-                    <Circle layoutId="circle" style={{ borderRadius: 50 }} />
+        <Wrapper>
+            <Grid>
+                {["1", "2", "3", "4"].map((n) => (
+                    <Box onClick={() => setId(n)} key={n} layoutId={n} />
+                ))}
+            </Grid>
+            <AnimatePresence>
+                {id ? (
+                    <Overlay
+                        variants={overlay}
+                        onClick={() => setId(null)}
+                        initial="hidden"
+                        animate="visible"
+                        exit="exit"
+                    >
+                        <Box layoutId={id} style={{ width: 400, height: 200 }} />
+                    </Overlay>
                 ) : null}
-            </Box>
-            <Box>
-                {clicked ? (
-                    <Circle layoutId="circle" style={{ borderRadius: 0, scale: 2 }} />
-                ) : null}
-            </Box>
+            </AnimatePresence>
         </Wrapper>
     );
 }
